b06c188decf2c9d4203df0b13aeaea7fd9f07eac
Andrei <andrei.cristea@apicore.de>
2021-03-03 03:02:00 +0100

    style: 404 issues. add link to 404 everywhere(temporary)

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const pathnames = ['/', '/contact.html', '/404.html', '/redirected.html', '/index.html', '/about.html'];
+document.addEventListener("DOMContentLoaded", () => {
+    const pathnames = ["/", "/contact.html", "/404.html", "/redirected.html", "/index.html", "/about.html"];
     
-    console.log(location.pathname)
+    console.log(location.pathname);
     
-    if (location.pathname === '/') {
-    location.href = './index.html'
+    if (location.pathname === "/") {
+        location.href = "./index.html";
     }
     
     if (!pathnames.includes(location.pathname)) {
-    console.log(location.pathname);
-    location.href = './404.html'
+        console.log(location.pathname);
+        location.href = "./404.html";
     }
-    });
+});
