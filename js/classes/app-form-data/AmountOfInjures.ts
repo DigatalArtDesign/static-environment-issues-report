0ba6ec83210523e9080d530500ea223411d970aa
Andrei <andrei.cristea@apicore.de>
2021-03-29 01:21:27 +0200

    fix: propertyDeserializers caused issues for script dropdown to run

--- js/classes/app-form-data/AmountOfInjures.ts
+++ js/classes/app-form-data/AmountOfInjures.ts
@@ -15,14 +15,6 @@ export default class AmountOfInjures implements FormSerialisable<AmountOfInjures
       return Object.keys(i);
     }
   
-    static propertyDeserializers =
-    // eslint-disable-next-line no-unused-vars
-    new Map<string, ((value: any) => any) | undefined>([
-      ["minor", undefined],
-      ["medium", undefined],
-      ["severe", undefined]
-    ]);
-  
     public deserialise(amount: any): AmountOfInjures {
       const props: SerialisableProperties[] = AmountOfInjures.fields.map(i => ({propName: i}));
       const serialiser = new Deserialiser(AmountOfInjures, props);
